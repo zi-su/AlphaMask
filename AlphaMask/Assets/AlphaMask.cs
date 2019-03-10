f219b8315251d9bb813bd4cb8774fee9ada2f500
Masato Naito <aitona765@gmail.com>
2019-03-10 15:33:32 +0900

    ローカルスケーリング、複数ゲームオブジェクト対応

--- AlphaMask/Assets/AlphaMask.cs
+++ AlphaMask/Assets/AlphaMask.cs
@@ -8,13 +8,18 @@ public class AlphaMask : MonoBehaviour
     Image[] _images;
     [SerializeField]
     Texture2D _alphaTex;
-
+    [SerializeField]
+    Material _mat;
     RectTransform rect;
     // Start is called before the first frame update
     void Start()
     {
         rect = GetComponent<RectTransform>();
-        _images = GetComponentsInChildren<Image>();
+        _images = GetComponentsInChildren<Image>(true);
+        foreach (var item in _images)
+        {
+            item.material = new Material(_mat);
+        }
     }
 
     // Update is called once per frame
@@ -25,8 +30,8 @@ public class AlphaMask : MonoBehaviour
             item.material.SetTexture("_AlphaTex", _alphaTex);
             var pos = rect.worldToLocalMatrix.MultiplyVector(rect.localPosition);
             item.material.SetVector("_AlphaPos", rect.localPosition);
-            item.material.SetFloat("_Width", rect.sizeDelta.x);
-            item.material.SetFloat("_Height", rect.sizeDelta.y);
+            item.material.SetFloat("_Width" , rect.sizeDelta.x * rect.localScale.x);
+            item.material.SetFloat("_Height", rect.sizeDelta.y * rect.localScale.y);
         }
     }
 }
